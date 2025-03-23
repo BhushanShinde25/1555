808a9f0eb1c3f1e7a88ee2e5e4b40c55c26c4bb6
BhushanShinde <bhushannshinde7@gmail.com>
2025-03-23 10:35:50 +0530

    Deployment3

--- src/components/ModelCanvas.jsx
+++ src/components/ModelCanvas.jsx
@@ -1,6 +1,6 @@
+import { Suspense } from "react";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
 import { PondModel } from "../ModelView/PondModel";
 
 export default function ModelCanvas() {
@@ -10,21 +10,22 @@ export default function ModelCanvas() {
       style={{
         width: "450px",
         height: "450px",
-
         top: "-45px",
       }}
     >
       <ambientLight intensity={0.5} />
       <OrbitControls
         maxPolarAngle={Math.PI / 2}
-        autoRotate={true}
+        autoRotate
         autoRotateSpeed={1}
         maxDistance={6}
         minDistance={5}
       />
-      <group scale={4.5}>
-        <PondModel />
-      </group>
+      <Suspense fallback={<div>Loading...</div>}>
+        <group scale={4.5}>
+          <PondModel />
+        </group>
+      </Suspense>
       <Environment preset="sunset" />
     </Canvas>
   );
